9fb2f7f4a8e2e6c948b91eaf4f032b3a96ac1af0
Eriry930 <erika-915@hotmail.com>
2022-01-13 19:35:01 -0500

    modificacion de JavaScript y CSS

--- script.js
+++ script.js
@@ -6,19 +6,22 @@
 //const panels = document.getElementById('panel')
 //const panels = document.getElementsByClassName('.panel')
 
+const panels = document.querySelectorAll('.panel')
 
 
 panels.forEach((panel) => {
     //console.log(panel)
     panel.addEventListener('click', () => {
-        
+        removeActiveClasses();
         panel.classList.add('active')
-
-
     })
 
 })
 
 function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active');
+    })    
+    
     
 }
